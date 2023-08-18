58f046602bbe39ac05df0a66ba04665557ddbbed
delimmaemma <calvinhtml@gmail.com>
2023-08-17 19:58:13 -0500

    Buttons toggled enabled/disabled upon cancellation

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -184,7 +184,7 @@ export default function App(props) {
           <Route exact path="/" element={<LoginForm login={login}/>} />
           <Route exact path="articles" element={
             <>
-              <ArticleForm currentArticle={currentArticle} setCurrentArticleId={setCurrentArticle} postArticle={postArticle} updateArticle={updateArticle}/>
+              <ArticleForm currentArticle={currentArticle} setCurrentArticleId={setCurrentArticleId} postArticle={postArticle} updateArticle={updateArticle}/>
               <Articles currentArticleId={currentArticleId} setCurrentArticleId={setCurrentArticleId} articles={articles} deleteArticle={deleteArticle} updateArticle={updateArticle} getArticles={getArticles}/>
             </>
           } />
--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -15,15 +15,16 @@ export default function ArticleForm(props) {
   } = props
 
   useEffect(() => {
+    console.log(currentArticle)
     // ✨ implement
     // Every time the `currentArticle` prop changes, we should check it for truthiness:
     // if it's truthy, we should set its title, text and topic into the corresponding
     // values of the form. If it's not, we should reset the form back to initial values.
     if (currentArticle) {
       setValues({
-        title: currentArticle.title || '',
-        text: currentArticle.text || '',
-        topic: currentArticle.topic || '',
+        title: currentArticle.title,
+        text: currentArticle.text,
+        topic: currentArticle.topic,
       });
     } else {
       setValues(initialFormValues);
