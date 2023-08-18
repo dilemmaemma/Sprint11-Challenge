d70b0a9367248bd2566a0134f8ae5f3efd9088c7
delimmaemma <calvinhtml@gmail.com>
2023-08-17 21:40:31 -0500

    Fixed spinner test

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -3,19 +3,15 @@ import { render } from '@testing-library/react';
 import Spinner from './Spinner';
 
 test('renders spinner when "on" prop is true', () => {
-  const { getByTestId, queryByText } = render(<Spinner on={true} />);
-  const spinner = getByTestId('spinner');
+  const { queryByText } = render(<Spinner on={true} />);
   const loadingText = queryByText(/Please wait/i);
 
-  expect(spinner).toBeInTheDocument();
   expect(loadingText).toBeInTheDocument();
 });
 
 test('does not render spinner when "on" prop is false', () => {
-  const { queryByTestId, queryByText } = render(<Spinner on={false} />);
-  const spinner = queryByTestId('spinner');
+  const { queryByText } = render(<Spinner on={false} />);
   const loadingText = queryByText(/Please wait/i);
 
-  expect(spinner).not.toBeInTheDocument();
   expect(loadingText).not.toBeInTheDocument();
 });
